404eceea02d275387d90e181ad5404871be4352b
Leonardo13-20 <152093268+Leonardo13-20@users.noreply.github.com>
2024-02-24 23:00:18 -0500

    Update app.js actualizo el pueto mysql a 3306

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.use(
       host: "localhost",
       user: "id21425470_leonardophp",
       password: "Leo1320**",
-      
+      port: 3306,
       database: "id21425470_leonardo",
     },
     "single"
